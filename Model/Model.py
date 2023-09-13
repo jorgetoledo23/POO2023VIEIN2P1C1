0a8c364bf90701df6aa7b832b9d39d63a523985c
Jorge <jorge.toledo.6@hotmail.com>
2023-09-13 20:21:04 -0300

    13 Sept

--- Model/Model.py
+++ Model/Model.py
@@ -1,19 +1,91 @@
+import datetime
+import os
+
 class Usuario:
     #Atributos 
-    def __init__(self, cod, email, rol, nom, password, rut): #Constructor
-        if(isinstance(cod, int)):
-            self.Codigo = cod
-        else:
-            raise TypeError("El Codigo debe ser un numero Entero")
+    def __init__(self, email, nom, nomperfil, password): #Constructor
         self.Email = email
-        self.Rol = rol
         self.Nombre = nom
+        self.NombrePerfil = nomperfil
         self.Password = password
-        self.Rut = rut
+        self.Seguidores = []
+        self.Seguidos = []
+        self.Posts = []
+        self.BandejaEntrada = []
+    
+    def Seguir(self, seguido):
+        self.Seguidos.append(seguido)
+        seguido.Seguidores.append(self)
+
+    def EnviarMensaje(self, destinatario, mensaje):
+        M = Mensaje(self, destinatario, mensaje)
+        destinatario.BandejaEntrada.append(M)
+
+    def VerSeguidos(self):
+        os.system("cls")
+        print(f"tienes { len(self.Seguidos) } seguidos")
+        for S in self.Seguidos:
+            print(S.NombrePerfil)
+
+    def VerSeguidores(self):
+        os.system("cls")
+        print(f"tienes { len(self.Seguidores) } seguidores")
+        for S in self.Seguidores:
+            print(S.NombrePerfil)
+
+    def VerMensajes(self):
+        os.system("cls")
+        for M in self.BandejaEntrada:
+            print(f"De: {M.Remitente.NombrePerfil} Mensaje: {M.Mensaje}")
+            
+class Mensaje:
+    def __init__(self, user, destinatario, mensaje):
+        self.Remitente = user
+        self.Destinatario = destinatario
+        self.Mensaje = mensaje
+        self.Fecha = datetime.datetime.now()
+        self.Estado = "No Leido"
+
+colocolooficial = Usuario("colocolochile@cc.cl", "Colo Colo", "Colo Colo Oficial", "1234")
+messi = Usuario("messi@fifa.com", "Lionel Messi", "LioMessi", "1234")
+elbicho = Usuario("elbicho@cr7.com", "Cristiano Ronaldo", "Cristiano", "1234")
+m14 = Usuario("m14@chile.cl", "Matias Fernandez", "Matigol", "1234")
+
+# m14.EnviarMensaje(colocolooficial, "Paguen lo que me deben")
+# messi.EnviarMensaje(colocolooficial, "Contratenme por favor!")
+
+# colocolooficial.VerMensajes()
 
 
-Administrador = Usuario("1", "admin@gmail.com", "Admin", "Administrador", "123456", "1-1")
-Auxiliar = Usuario(2, "aux@gmail.com", "Auxiliar", "Veronica", "1234", "2-K")
+listaUsuarios = [colocolooficial, messi, elbicho, m14]
 
-input("Ingresa Email: ")
-print(Administrador.Email)
+while True:
+    os.system("cls")
+    print("[1] - Registro")
+    print("[2] - Iniciar Sesion")
+    opcion = input("Selecciona una Opcion: ")
+    if(opcion == "1"):
+        os.system("cls")
+        n = input("Ingresa Nombre: ")
+        c = input("Ingresa Correo: ")
+        u = input("Ingresa Nombre de Perfil: ")
+        p = input("Ingresa una Contraseña: ")
+        newUser = Usuario(c, n, u, p)
+        listaUsuarios.append(newUser)
+        input("Usuario registrado!")
+    if(opcion == "2"):
+        os.system("cls")
+        correo = input("Ingresa tu Correo: ")
+        user = None
+        for U in listaUsuarios:
+            if(U.Email == correo):
+                user = U
+        if(user == None):
+            input("No Existe ese Usuario")
+        else:
+            os.system("cls")
+            contra = input("Ingrese Contraseña: ")
+            if(user.Password == contra):
+                input("Inicio de Sesion Correcto!")
+            else:
+                input("Contraseña Incorrecta")
